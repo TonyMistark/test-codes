e9b7cff33a07a258c25c909e566fb7dc8292267f
ice <tony_mistark@163.com>
2016-08-30 18:32:05 +0800

    add iter-test

---
+++ iter_yield.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+class TestIter(object):
+
+    def __init__(self):
+        self.lst = [1,2,3,4,5,6,8,7]
+
+    def read(self):
+        for i in xrange(len(self.lst)):
+            yield i
+
+    def __iter__(self):
+        return self.read()
+
+    def __str__(self):
+        return ",".join(map(str, self.lst))
+    __repr__ = __str__
+
+def test_iter():
+    obj = TestIter()
+    for num in obj:
+        print "num", num
+    print "obj", obj
+
+if __name__ == "__main__":
+    test_iter()
